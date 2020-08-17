c19a40a45b0912e357b2bb7d7e02c59c15419b82
tomhash <tomonarihashikami@gmail.com>
2020-08-17 12:31:43 +0900

    新規登録ログインログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+    def configure_permitted_parameters
+      added_attrs = [:nickname,:email,:password,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday]
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+   end
 
-  def basic_auth
+    def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname,  presence: true
+  validates :email, presence: true
+  
+  validates :password, presence: true
+  
+  validates :family_name,  presence: true
+  validates :first_name,  presence: true
+    
+  validates :family_name_kana,  presence: true
+  validates :first_name_kana,  presence: true
+  validates :family_name_kana,:first_name_kana, format: {
+    with: /\A[\p{katakana} --&&[^ -~｡-ﾟ]]+/} 
+  validates :birthday,  presence: true  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
