8afe0dd85dbf814bec984a7a539042d73c0f6e70
tomhash <tomonarihashikami@gmail.com>
2020-08-18 14:26:44 +0900

    editユーザー管理機能の実装2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   with_options presence: true do
+    validates :birthday,:nickname
     validates :family_name,:first_name, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/}
     validates :family_name_kana,:first_name_kana, format: {with: /\A[\p{katakana} --&&[^ -~｡-ﾟ]]+/ }
+
   end
-  validates :birthday,  presence: true  
+    
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
