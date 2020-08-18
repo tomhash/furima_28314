12e5f5de3445ae68f824c8f1977cc7ae185bf90b
tomhash <tomonarihashikami@gmail.com>
2020-08-18 11:06:30 +0900

    ユーザー管理テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :nickname,  presence: true
   validates :email, presence: true
-  
   validates :password, presence: true
-  
+  validates :password, length: { minimum: 6 }
   validates :family_name,  presence: true
   validates :first_name,  presence: true
-    
   validates :family_name_kana,  presence: true
   validates :first_name_kana,  presence: true
+  validates :family_name,:first_name, format: {
+    with: /\A[ぁ-んァ-ン一-龥]+\z/}
   validates :family_name_kana,:first_name_kana, format: {
     with: /\A[\p{katakana} --&&[^ -~｡-ﾟ]]+/} 
   validates :birthday,  presence: true  
