eb457e572e18e0b7febf2a574eca905081252a6e
tomhash <tomonarihashikami@gmail.com>
2020-08-21 14:29:35 +0900

    出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
-def index
-end  
+ before_action :authenticate_user!,only: [:new,:create]
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+   
+  end
+  
+  def show
+  end
+  def destroy
+  end
+  
+  def item_params
+    params.require(:item).permit(:image,:name,:price,:description,
+      :category_id,:shipping_type_id,:item_condition_id,:ship_from_id,:preparation_day_id).merge(user_id: current_user.id)
+  end
 end
--- db/migrate/20200813044447_create_items.rb
+++ db/migrate/20200813044447_create_items.rb
@@ -1,12 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
-      # t.integer    :category_id           , null: false
-      # t.integer    :item_condition_id     , null: false
-      # t.integer    :preparation_day_id    , null: false
-      # t.integer    :ship_from_id          , null: false
-      # t.integer    :shipping_type_id      , null: false
+      t.string     :name                  , null: false
+      t.string     :description           , null: false
+      t.integer    :category_id           , null: false
+      t.integer    :item_condition_id     , null: false
+      t.integer    :shipping_type_id      , null: false
+      t.integer    :ship_from_id          , null: false
+      t.integer    :preparation_day_id    , null: false 
+      t.integer    :price                 , null: false
+      t.references :user                  ,foreign_key:true
       t.timestamps
     end
   end
