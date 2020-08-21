a43d8c9635d8326ea9b8234ac10f0bfde05b8ab2
tomhash <tomonarihashikami@gmail.com>
2020-08-21 16:00:48 +0900

    edit商品出品機能１

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,22 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category 
-  belongs_to_active_hash :item_condition
-  belongs_to_active_hash :preparation_day
-  belongs_to_active_hash :ship_from
-  belongs_to_active_hash :shipping_type
-  validates :name, :description, :category_id, :item_condition_id, :preparation_day, :ship_from_id, :shipping_type_id, :price, presence: true
-  validates :category_id, :item_condition_id, :preparation_day_id, :ship_from_id, :shipping_type_id, numericality: { other_than: 1 } 
-
-  belongs_to :user
-  has_one_attached :image
+  with_options presence: true do
+    belongs_to_active_hash :category 
+    belongs_to_active_hash :item_condition
+    belongs_to_active_hash :preparation_day
+    belongs_to_active_hash :ship_from
+    belongs_to_active_hash :shipping_type
+    validates :name, :description 
+ end
+  with_options numericality: { other_than: 1 }  do
+    belongs_to_active_hash :category 
+    belongs_to_active_hash :item_condition
+    belongs_to_active_hash :preparation_day
+    belongs_to_active_hash :ship_from
+    belongs_to_active_hash :shipping_type
+ end
+   validates :price,  presence: true, inclusion: { in: 300..9999999 }
+  
+   belongs_to :user
+   has_one_attached :image
 end
