82c1a3d87acc91af47681fe4a64816ce68cf5dd5
tomhash <tomonarihashikami@gmail.com>
2020-08-23 17:42:36 +0900

    Update buyer.rb

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,8 +1,5 @@
 class Buyer < ApplicationRecord
-  with_options foreign_key: true do
-  t.references :user 
-  t.references :item
-  end
+  
   belongs_to :user
   belongs_to :item
   has_one :destination
