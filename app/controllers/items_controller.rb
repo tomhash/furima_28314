b600560e543cc0db1f68a764acccb9dcb423ca19
tomhash <tomonarihashikami@gmail.com>
2020-08-23 21:06:36 +0900

    商品詳細の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
  before_action :authenticate_user!,only: [:new,:create]
+
   def index
     
     @items = Item.all.order("created_at DESC")
@@ -20,6 +21,7 @@ class ItemsController < ApplicationController
   end
   
   def show
+    @item = Item.find(params[:id])
 
   end
   def destroy
