86fcace16c53a495c6da5a27263112f6c3a44922
tomhash <tomonarihashikami@gmail.com>
2020-08-24 18:34:51 +0900

    商品削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
- before_action :authenticate_user!,only: [:new,:create,:edit]
- before_action :set_item,only: [:show,:edit,:update]
+ before_action :authenticate_user!,only: [:new,:create,:edit,:destroy]
+ before_action :set_item,only: [:show,:edit,:update,:destroy]
   def index
     
     @items = Item.all.order("created_at DESC")
@@ -37,6 +37,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
